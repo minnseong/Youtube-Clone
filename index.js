6a8a8c08a0b1d719f3cb4bffff5cff9c66efdc91
Kang-Min-Seong <kdsvip5@naver.com>
2021-01-22 01:55:41 +0900

    using babel

--- index.js
+++ index.js
@@ -1,19 +1,17 @@
 // require : node module를 어디선가 가져오는 역할
-const express = require('express')
+// const express = require('express')
+import express from "express";
 const app = express()
+
 const PORT = 4000
 
 function handleListening() {
     console.log(`Listening on : http://localhost:${PORT}`);
 }
+// arrow function
+const handleProfile = (req, res) => res.send("You are on my profile!");
+const handleHome = (req, res) => res.send("hello from home");
 
-function handleHome(req, res) {
-    res.send('hello from home');
-}
-
-function handleProfile(req, res) {
-    res.send("You are on my profile")
-}
 
 // app.get home(/) 접근시, handleHome function call
 app.get("/", handleHome);
