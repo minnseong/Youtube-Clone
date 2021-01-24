2b4dd3496de8620af92538c3803f0244dabe9ce8
Kang-Min-Seong <kdsvip5@naver.com>
2021-01-25 03:08:31 +0900

    Search Controller

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -1,7 +1,15 @@
 export const home = (req, res) => res.render("home", { pageTitle : "Home" });
-export const search = (req, res) => res.render("search", { pageTitle : "Seach" });
+
+export const search = (req, res) => {
+    //const searchingBy = req.query.term;
+    const {query: { term: searchingBy }} = req;
+    res.render("search", { pageTitle : "Seach" , searchingBy});
+};
 
 export const upload = (req, res) => res.render("upload", { pageTitle : "Upload" });
+
 export const videoDetail = (req, res) => res.render("videoDetail", { pageTitle : "Video Detail" });
+
 export const editVideo = (req, res) => res.render("editVideo", { pageTitle : "Edit Video" });
+
 export const deleteVidoe = (req, res) => res.render("deleteVideo", { pageTitle : "Delete Video" });
