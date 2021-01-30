0d8994993fa9170b833ca90e79be6047451bd549
Kang-Min-Seong <kdsvip5@naver.com>
2021-01-30 15:08:25 +0900

    Edit and Delete Video

--- routers/videoRouter.js
+++ routers/videoRouter.js
@@ -1,15 +1,22 @@
 import express from "express";
 import routes from "../routes";
-import { postUpload ,deleteVidoe, editVideo, getUpload, videoDetail} from "../controllers/videoController";
+import { postUpload ,deleteVidoe, getUpload, videoDetail, getEditVideo, postEditVideo} from "../controllers/videoController";
 import { uploadVideo } from "../middlewares";
 
 const videoRouter = express.Router();
 
+// Upload
 videoRouter.get(routes.upload, getUpload);
 videoRouter.post(routes.upload, uploadVideo, postUpload);
 
+// Video Detail
 videoRouter.get(routes.videoDetail(), videoDetail);
-videoRouter.get(routes.editVideo, editVideo);
-videoRouter.get(routes.deleteVideo, deleteVidoe);
+
+// Edit Video
+videoRouter.get(routes.editVideo(), getEditVideo);
+videoRouter.post(routes.editVideo(), postEditVideo);
+
+// Delete Video
+videoRouter.get(routes.deleteVideo(), deleteVidoe);
 
 export default videoRouter;
