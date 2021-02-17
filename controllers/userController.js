de3a86b33a57c020f4f52b60c5c6597c16f27e0a
Kang-Min-Seong <kdsvip5@naver.com>
2021-02-17 18:33:24 +0900

    User Profile

--- controllers/userController.js
+++ controllers/userController.js
@@ -93,5 +93,5 @@ export const userDetail = async(req, res) => {
     }
 }
 
-export const editProfile = (req, res) => res.render("editProfile", { pageTitle : "Edit Profile" });
+export const getEditProfile = (req, res) => res.render("editProfile", { pageTitle : "Edit Profile" });
 export const changePassword = (req, res) => res.render("changePassword", { pageTitle : "Change Pwd" });
